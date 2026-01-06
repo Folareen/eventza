1fcbe903082ead158d1639f46b5581659fa5584b
Folareen <sakawahab03@gmail.com>
2026-01-06 13:07:43 +0100

    fixed weird sequelize issue

--- backend/index.ts
+++ backend/index.ts
@@ -29,9 +29,6 @@ const PORT = process.env.PORT || 3000;
         await sequelize.authenticate();
         console.log('Database connection established successfully.');
 
-        // await sequelize.sync();
-        // console.log('Database synchronized.');
-
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,10 +1,13 @@
 import { Sequelize } from 'sequelize';
-
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
-const sequelize = new Sequelize(DB_NAME!, DB_USER!, DB_PASSWORD!, {
+if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_PORT) {
+    throw new Error('Database configuration is missing');
+}
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
-    port: parseInt(DB_PORT!),
+    port: parseInt(DB_PORT),
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
