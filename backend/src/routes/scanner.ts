b9bad81fb06461194c3a12fae5738c30f82b4a48
Folareen <sakawahab03@gmail.com>
2026-01-15 06:23:57 +0100

    refactor

--- backend/src/routes/scanner.ts
+++ backend/src/routes/scanner.ts
@@ -1,15 +1,24 @@
 import { Router } from 'express';
-import { validate } from '../middleware/validate';
-import { scannerLogin } from '../controllers/scanner/scannerLogin';
-import { getScannerEvents } from '../controllers/scanner/getScannerEvents';
 import { checkInTicket } from '../controllers/scanner/checkInTicket';
-import { scannerLoginSchema } from '../validators/scanner';
+import { createScanner } from '../controllers/scanner/createScanner';
+import { deleteScanner } from '../controllers/scanner/deleteScanner';
+import { getScannerEvents } from '../controllers/scanner/getScannerEvents';
+import { scannerLogin } from '../controllers/scanner/scannerLogin';
+import { updateScanner } from '../controllers/scanner/updateScanner';
+import { authenticateScanner } from '../middleware/authenticate/scanner';
+import { authenticateUser } from '../middleware/authenticate/user';
+import { validate } from '../middleware/validate';
+import { createScannerSchema, scannerLoginSchema, updateScannerSchema } from '../validators/scanner';
 
 const router = Router();
 
 router.post('/login', validate(scannerLoginSchema, 'body'), scannerLogin);
 
-router.get('/:scannerId/events', getScannerEvents);
-router.post('/:eventId/checkin', checkInTicket);
+router.post('/scanners', authenticateUser, validate(createScannerSchema, 'body'), createScanner);
+router.put("/me/scanners/:scannerId", authenticateUser, validate(updateScannerSchema, 'body'), updateScanner);
+router.delete("/me/scanners/:scannerId", authenticateUser, deleteScanner);
+
+router.get('/:scannerId/events', authenticateScanner, getScannerEvents);
+router.post('/:eventId/checkin', authenticateScanner, checkInTicket);
 
 export default router;
