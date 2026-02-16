db1172242d466a6a00f48f53b7fde21c2107a875
Folareen <sakawahab03@gmail.com>
2026-02-16 09:39:31 +0100

    cleaunp

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import '@mantine/core/styles.css';
-
-import { MantineProvider } from '@mantine/core';
-
-export default function App() {
-  return <MantineProvider>
-    Eventza
-  </MantineProvider>;
-}
